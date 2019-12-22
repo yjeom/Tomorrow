e93a9671f2362b05e24ba8db8b4749e77caf39aa
yjeom <44234192+yjeom@users.noreply.github.com>
2019-12-22 21:48:07 +0900

    Create MemberService.java

---
+++ src/main/java/com/spring/tomorrow/member/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.spring.tomorrow.member.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.spring.tomorrow.member.vo.MemberVO;
+
+public interface MemberService {
+	 public List listMembers() throws DataAccessException;
+	 public int addMember(MemberVO memberVO) throws DataAccessException;
+	 public int removeMember(String id) throws DataAccessException;
+	 public MemberVO login(MemberVO memberVO) throws Exception;
+}
